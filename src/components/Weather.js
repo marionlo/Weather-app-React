3074ce3752b3103974d6562a68e0d8514b50cedb
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 21:43:02 +0200

    Add props on the weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,13 @@ import React from "react";
 class Weather extends React.Component {
   render () {
     return (
-      <div>Weather Component</div>
+      <div>
+      { this.props.city }
+      { this.props.country }
+      { this.props.temperature }
+      { this.props.humidity }
+      { this.props.description }
+      </div>
     );
   }
 }
