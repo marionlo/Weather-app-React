12b8c8d73c43d26fafb1df447fc39e58693de862
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 20:21:14 +0200

    add api data converted into json

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Weather from "./components/Weather";
 const API_KEY = "d507952f39f004fe8b4abb451607bca1";
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dijon,fr&mode=json&appid=${API_KEY}&units=metric`)
+  }
+
   render () {
     return (
       <div>
