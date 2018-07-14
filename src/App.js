64628a336339f26948ba596a8f7ce005ef81db18
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 20:25:30 +0200

    Add Form on Form Component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const API_KEY = "d507952f39f004fe8b4abb451607bca1";
 
 class App extends React.Component {
   getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dijon,fr&mode=json&appid=${API_KEY}&units=metric`)
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dijon,fr&mode=json&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
   }
 
   render () {
