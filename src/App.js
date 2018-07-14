d2febfddf74a31389113370047ce39e4d410e495
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 21:23:35 +0200

    Add getweather function on submit button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Weather from "./components/Weather";
 const API_KEY = "d507952f39f004fe8b4abb451607bca1";
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dijon,fr&mode=json&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
   }
@@ -15,7 +16,7 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
