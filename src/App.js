7177e2025384002e05349e6a5dfd07f1939c5238
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 20:04:35 +0200

    Add Titles component on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import Titles from "./components/Titles";
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <p>Hello World</p>
-      </div>
+      <Titles />
     );
   }
 };
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-Class Titles extends React.Component {
+class Titles extends React.Component {
   render () {
     return (
       <div>
-        <h1>Weather Finder</h>
+        <h1>Weather Finder</h1>
         <p>Find out temperature, conditions and more...</p>
       </div>
     );
   }
-}
+};
 
 export default Titles;
