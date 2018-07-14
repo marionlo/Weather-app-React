6cb059217ed52f23dd083458016128a994924439
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 21:51:50 +0200

    only render when city and country are filled

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,8 +6,8 @@ class Weather extends React.Component {
       <div>
       { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
       {this.props.temperature &&   <p>Temperature: { this.props.temperature }</p>}
-      {this.props.humidity && <p>Humidity:   { this.props.humidity }</p>}
-      {this.props.description && <p>Conditions:   { this.props.description }</p>}
+      {this.props.humidity && <p>Humidity: { this.props.humidity }</p>}
+      {this.props.description && <p>Conditions: { this.props.description }</p>}
 
       </div>
     );
