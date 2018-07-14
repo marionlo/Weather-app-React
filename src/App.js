b4ec8c392edb9dd870b9309721621d505fb5ea07
Marion-Lotito <marion.lotito@gmail.com>
2018-07-14 20:11:42 +0200

    add API key weather

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "d507952f39f004fe8b4abb451607bca1";
+
 class App extends React.Component {
   render () {
     return (
